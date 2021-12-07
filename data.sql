526e7e0304d15f5026d4be4f7259e826b74f4efd
Ahmet <ahmt9417@gmail.com>
2021-12-07 15:53:25 +0300

    Add data.sql file

---
+++ data.sql
@@ -0,0 +1,21 @@
+INSERT INTO
+  animals (name)
+VALUES
+  ('Agumon'),
+  ('Gabumon'),
+  ('Pikachu'),
+  ('Devimon'),
+  ('Charmander'),
+  ('Plantmon'),
+  ('Squirtle'),
+  ('Angemon'),
+  ('Boarmon'),
+  ('Blossom');
+
+INSERT INTO
+  vets (name)
+VALUES
+  ('William Tatcher'),
+  ('Maisy Smith'),
+  ('Stephanie Mendez'),
+  ('Jack Harkness');
